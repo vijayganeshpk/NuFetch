8189db2ed4b577fb4bb6889a648178d09944b72e
Vijay Ganesh <vijay@vijay.name>
2015-08-24 21:57:56 +0400

    Added logging

---
+++ NuFetch/NuFetchExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NuFetch {
+    static class NuFetchExtensions {
+        public static string ToJson(this object val) {
+            var settings = new JsonSerializerSettings {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            return JsonConvert.SerializeObject( val, Formatting.Indented, settings );
+        }
+    }
+}
--- NuFetch/Program.cs
+++ NuFetch/Program.cs
@@ -1,19 +1,30 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using NLog;
 
 namespace NuFetch {
     class Program {
+        readonly static Logger log = LogManager.GetCurrentClassLogger();
         static void Main( string[] args ) {
             MainAsync( args ).Wait();
         }
 
         static async Task MainAsync(string[] args) {
             await Task.Run( () => {
+                log.Trace( $"Entered MainAsync(string[] args='{string.Join(", ", args )}')" );
+
                 var appOptions = new ApplicationOption();
                 var parseResult = CommandLine.Parser.Default.ParseArguments(args, appOptions);
+
+                log.Trace( $"arguments parsing was successful? {parseResult}" );
 
                 if( !parseResult ) {
                     return;
                 }
+
+                log.Debug( $"Application options: {appOptions.ToJson()}" );
+
+                log.Trace( "Exiting MainAsync" );
             } );
         }
     }
