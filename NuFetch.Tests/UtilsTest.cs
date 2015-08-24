569b6b474fa8750d58dda0c4a1eec5a490b1d934
Vijay Ganesh <vijay@vijay.name>
2015-08-24 22:13:29 +0400

    Adding utility class and related tests

---
+++ NuFetch.Tests/UtilsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuFetch.Tests {
+    [TestClass]
+    public class UtilsTest {
+        [TestMethod]
+        public void GetFullFolderPath_Returns_AbsoultePath_For_AbsolutePath() {
+            var initialFolderPath = @"C:\Temp\NuFetch";
+            var finalFolderPath = Utils.GetFullFolderPath(initialFolderPath);
+
+            Assert.AreEqual( initialFolderPath, finalFolderPath );
+        }
+    }
+}
---
+++ NuFetch/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NuFetch {
+    public static class Utils {
+        public static string GetFullFolderPath( string folderPath ) {
+            return string.Empty;
+        }
+    }
+}
