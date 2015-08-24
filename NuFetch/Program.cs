ff1fc9d0368628880ce7b45dcd90d706670c5d22
Vijay Ganesh <vijay@vijay.name>
2015-08-24 21:38:53 +0400

    Comand line parser integerated with application start

--- NuFetch/Program.cs
+++ NuFetch/Program.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace NuFetch {
     class Program {
         static void Main( string[] args ) {
+            MainAsync( args ).Wait();
         }
 
         static async Task MainAsync(string[] args) {
-            await Task.Run( () => { } );
+            await Task.Run( () => {
+                var appOptions = new ApplicationOption();
+                var parseResult = CommandLine.Parser.Default.ParseArguments(args, appOptions);
+
+                if( !parseResult ) {
+                    return;
+                }
+            } );
         }
     }
 }
