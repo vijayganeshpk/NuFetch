505f0c7f4f53fd865f9a8a15af75b838db702725
Vijay Ganesh <vijay@vijay.name>
2015-09-01 17:18:27 +0400

    Corrected spellings, and grammar

--- src/NuFetchLib/ApplicationOption.cs
+++ src/NuFetchLib/ApplicationOption.cs
@@ -6,7 +6,7 @@ namespace NuFetchLib {
         [Option('s',"source", DefaultValue = "https://www.nuget.org/api/v2/", HelpText = "NuGet repository source server URL")]
         public string ServerSource { get; set; }
 
-        [Option('p',"pid", Required = true, HelpText = "Package Id that needs to be downloaded with dependecies")]
+        [Option('p',"pid", Required = true, HelpText = "Package Id to download" )]
         public string PackageId { get; set; }
 
         [Option('v',"version", DefaultValue = null, HelpText = "Package Version to download; do not mention this parameter to donwload the latest version")]
@@ -21,7 +21,7 @@ namespace NuFetchLib {
         [Option('o',"overwrite", DefaultValue = false, HelpText = "Flag to overwrite existing file matching a package version")]
         public bool OverwriteExistingFiles { get; set; }
 
-        [Option('i',"includePrelease", DefaultValue = false, HelpText = "Flag denoting if the downloader should include prereleases when searching for package")]
+        [Option('i',"includePrelease", DefaultValue = false, HelpText = "Flag denoting if the downloader should include prerelease packages when searching for package")]
         public bool IncludePreRelease { get; set; }
 
         [Option('a',"allowUnlisted", DefaultValue = false, HelpText = "Flag denoting if the downloader should search for package in unlisted packages")]
